71a53db977de459ac6329520fd7153a97a01a976
yash raut <jayashwap236@gmail.com>
2023-05-27 21:42:20 +0530

    Calculator Multiply Method

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,14 @@
 public class Calculator {
     
 
+    public static void multiply(int a,int b){
+        System.out.println(a*b);
+    }
+
     public static void main(String[] args) {
         
         System.out.println("Calculator Method !!!!");
+
+        multiply(12, 2);
     }
 }
